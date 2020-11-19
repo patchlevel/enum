d3b8095df109e3f736003835e6d85e3a860491d9
Daniel Badura <d.m.badura@googlemail.com>
2020-11-19 20:07:56 +0100

    add missing direction example

---
+++ example/Direction.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Patchlevel\Enum\Example;
+
+use const JSON_THROW_ON_ERROR;
+use JsonSerializable;
+use Patchlevel\Enum\Enumerated;
+use function json_encode;
+
+/**
+ * @psalm-immutable
+ * @method static self up()
+ * @method static self down()
+ * @method static self left()
+ * @method static self right()
+ */
+final class Direction implements JsonSerializable
+{
+    use Enumerated;
+
+    private const UP = 'up';
+    private const DOWN = 'down';
+    private const LEFT = 'left';
+    private const RIGHT = 'right';
+}
+
+$directionUp = Direction::up();
+
+// this will result int the string "up"
+$encodedDirectionUp = json_encode($directionUp, JSON_THROW_ON_ERROR);
